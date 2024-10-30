036c8761bad1e9c213fbdcc316b106baadbd8358
Middlewarer <mranarchy@mail.ru>
2024-10-30 18:37:23 +0300

    Added new excellent changes

--- pythonstek/python/models.py
+++ pythonstek/python/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    descrption = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+    
+class UserProgress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    progress = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.progress}%"
