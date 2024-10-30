dbbd33e3b94d48356fc22efa0bfe70d2bbd5995d
Nikita Gromov <nikita.gromow.2000@gmail.com>
2024-10-30 20:12:00 +0300

    Commmit

--- pythonstek/python/views.py
+++ pythonstek/python/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = 'python/index.html'
+    template_name = 'python/purchase.html'
